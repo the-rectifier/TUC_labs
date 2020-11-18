1e245bc162b917c2127845db4647a99132db5363
odysseas stavrou <odysseassu@gmail.com>
2020-11-18 03:21:52 +0200

    Logs are working

--- 5th_Sem/ACE414_Labs/Exercise_4/test_aclog.c
+++ 5th_Sem/ACE414_Labs/Exercise_4/test_aclog.c
@@ -1,38 +1,68 @@
 #include <stdio.h>
 #include <string.h>
-
-int main() 
-{
-//	int i;
-//	size_t bytes;
-//	FILE *file;
-//	char filenames[10][7] = {"file_0", "file_1",
-//			"file_2", "file_3", "file_4",
-//			"file_5", "file_6", "file_7",
-//			"file_8", "file_9"};
-//
-//
-//	/* example source code */
-//
-//	for (i = 0; i < 10; i++) {
-//
-//		file = fopen(filenames[i], "w+");
-//		if (file == NULL)
-//			printf("fopen error\n");
-//		else {
-//			bytes = fwrite(filenames[i], strlen(filenames[i]), 1, file);
-//			fclose(file);
-//		}
-//
-//	}
-
-    FILE * fp = fopen("trin123ki", "r");
-	fclose(fp);
-	/* add your code here */
-	/* ... */
-	/* ... */
-	/* ... */
-	/* ... */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 
+int main(){
+	int i;
+	FILE *file;
+	char * junk = "junk junk junk";
+
+	char filenames[10][7] = {"file_0", "file_1",
+			"file_2", "file_3", "file_4",
+			"file_5", "file_6", "file_7",
+			"file_8", "file_9"};
+
+
+	/* Create all the files */
+	for(i = 0; i < 10; i++){
+		file = fopen(filenames[i], "w+");
+		if(file == NULL)
+			printf("fopen error\n");
+		else{
+			fwrite(filenames[i], strlen(filenames[i]), 1, file);
+			fclose(file);
+		}
+	}
+	
+	/* access file_0 and file_1 10 times with no permissions */
+	/* Expecting to see 20 denied access logs*/
+	chmod(filenames[0], 0);
+	chmod(filenames[1], 0);
+	for(int i=0;i<10;i++){
+		file = fopen(filenames[0], "r");
+		file = fopen(filenames[1], "r");
+	}
+
+	/* edit file_2 and file_3 10 times */
+	/** Expecting 12 hash changes
+	 * 1 for the open with write
+	 * 10 for the writing
+	 * 1 for the write at the end
+	*/
+	file = fopen(filenames[2], "w");
+	for(int i=0;i<10;i++){
+		fwrite(junk, strlen(junk)-1, 2, file);
+	}
+	fwrite(junk, strlen(junk), 1, file);
+	fclose(file);
+
+	/* same as above */
+	file = fopen(filenames[3], "wb");
+	for(int i=0;i<10;i++){
+		fwrite(junk, strlen(junk)-1, 1, file);
+	}
+	fwrite(junk, strlen(junk), 1, file);
+	fclose(file);
+
+	/* read file_4 */
+
+	file = fopen(filenames[4], "r");
+	char buffer[1024];
+	fread(buffer, 69, 1, file);
+	fclose(file);
+
+	return 0;
 }
