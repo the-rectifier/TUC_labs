13d5ac91c9a666b852715e86668ca728c3b34c44
odysseas stavrou <odysseassu@gmail.com>
2020-12-02 06:15:26 +0200

    Finished Project 5

--- 5th_Sem/ACE414_Labs/Exercise_4_5/acmonitor.c
+++ 5th_Sem/ACE414_Labs/Exercise_4_5/acmonitor.c
@@ -8,9 +8,50 @@
 
 #include "entry.h"
 
-const char * get_ext(const char *);
-void list_all_crypted(FILE *);
-int get_lines(FILE * fp);
+const char * get_ext(const char * filename){
+	const char * suffix = strrchr(filename, '.');
+	if(suffix == NULL || suffix == filename){
+		return "";
+	}
+	return suffix;
+}
+
+int get_lines(FILE * fp){
+	int lines = 0;
+	char ch;
+	
+	while(!feof(fp)){
+		ch = fgetc(fp);
+		if(ch == '\n')
+			lines++;
+	}
+	rewind(fp);
+	return lines;
+}
+
+void list_all_crypted(FILE * fp){
+	// const char delim[] = ".";
+	// const char suffix[] = ".encrypt";
+	int access_type;
+	int lines = get_lines(fp);
+	char path[PATH_MAX] = {0};
+	int count = 0;
+
+	for(int i=0;i<lines;i++){
+		fscanf(fp,"%*d\t%s\t%*d\t%d\t%*02d-%*02d-%*d\t%*02d:%*02d:%*02d\t%*s\n",path, &access_type);
+		if(!strcmp(get_ext(path), ".encrypt") && access_type == 0){
+			count++;
+			char * ptr = strtok(path, ".");
+			while(ptr != NULL){
+				if(strcmp(ptr, "encrypt"))
+					printf("%s", ptr);
+				ptr = strtok(NULL, ".");
+			}
+			putchar('\n');
+		}	
+	}
+	printf("Total # of files encrypted: %d\n", count);
+}
 
 void usage(void) {
 	printf(
@@ -154,6 +195,53 @@ void list_file_modifications(FILE *log, char *file_to_scan){
 
 }
 
+void list_20_mins(FILE * log, int threshold){
+	time_t time_now = time(NULL);
+	time_t file_time;
+	struct tm * file_time_tm = (struct tm *)malloc(sizeof(struct tm));
+	int count = 0;
+
+	int access_type;
+	int action_denied;
+	int day, month, year;
+	int hour, min, sec;
+	char filename[PATH_MAX] = {0};
+
+	int lines = get_lines(log);
+
+	for(int i=0;i<lines;i++){
+		fscanf(log,
+            "%*d\t%s\t%d\t%d\t%02d-%02d-%d\t%02d:%02d:%02d\t%*s\n",
+			filename,
+			&action_denied,
+			&access_type,
+			&day, &month, &year,
+			&hour, &min, &sec);
+
+		if(!action_denied && access_type == 0){
+			file_time_tm->tm_mday = day;
+			file_time_tm->tm_mon = month - 1;
+			file_time_tm->tm_year = year - 1900;
+			file_time_tm->tm_hour = hour;
+			file_time_tm->tm_min = min;
+			file_time_tm->tm_sec = sec;
+
+			file_time = mktime(file_time_tm);
+			if((difftime(time_now, file_time) / 60) <= 20){
+				printf("%s\n", filename);
+				count++;
+			}
+		}
+
+	}
+
+	free(file_time_tm);
+	printf("Total files created in the last 20 mins: %d\n", count);
+	if(count > threshold){
+		printf("SUPICIOUS ACTIVITY\n");
+	}
+
+}
 
 int main(int argc, char *argv[]){
 
@@ -169,7 +257,7 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	while ((ch = getopt(argc, argv, "hi:me")) != -1) {
+	while ((ch = getopt(argc, argv, "hi:mev:")) != -1) {
 		switch (ch) {		
 		case 'i':
 			list_file_modifications(log, optarg);
@@ -179,6 +267,9 @@ int main(int argc, char *argv[]){
 			break;
 		case 'e':
 			list_all_crypted(log);
+			break;
+		case 'v':
+			list_20_mins(log, atoi(optarg));
 			break;
 		default:
 			usage();
@@ -240,50 +331,4 @@ void add_failure(struct user * user, char * file){
 	if(user->access_fail == 8){
 		user->flagged = 1;
 	}
-}
-
-const char * get_ext(const char * filename){
-	const char * suffix = strrchr(filename, '.');
-	if(suffix == NULL || suffix == filename){
-		return "";
-	}
-	return suffix;
-}
-
-void list_all_crypted(FILE * fp){
-	const char delim[] = ".";
-	const char suffix[] = ".encrypt";
-	int lines = get_lines(fp);
-	char path[PATH_MAX] = {0};
-	char old_path[PATH_MAX] = {0};
-
-	for(int i=0;i<lines;i++){
-		fscanf(fp,"%*d\t%s\t%*d\t%*d\t%*02d-%*02d-%*d\t%*02d:%*02d:%*02d\t%*s\n",path);
-		if(strcmp(path, old_path)){
-			memcpy(old_path, path, PATH_MAX);
-			if(!strcmp(get_ext(path),suffix)){
-				char * ptr = strtok(path, delim);
-				while(ptr != NULL){
-					if(strcmp(ptr, "encrypt"))
-						printf("%s", ptr);
-					ptr = strtok(NULL, delim);
-				}
-				putchar('\n');
-			}	
-		}
-	}
-
-}
-
-int get_lines(FILE * fp){
-	int lines = 0;
-	char ch;
-	
-	while(!feof(fp)){
-		ch = fgetc(fp);
-		if(ch == '\n')
-			lines++;
-	}
-	rewind(fp);
-	return lines;
 }
